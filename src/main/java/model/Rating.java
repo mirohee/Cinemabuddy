9ca5bf8f2e69ca4d30bec115e6f2e6c605e4827a
BasedAksu <akselias02@gmail.com>
2024-03-14 18:29:23 +0200

    added rating mode

---
+++ src/main/java/model/Rating.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Rating {
+    private double average;
+
+    public double getAverage() {
+        return average;
+    }
+
+    public void setAverage(double average) {
+        this.average = average;
+    }
+}
+
